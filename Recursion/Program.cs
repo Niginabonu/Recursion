b9350cab756c116fe5dadf557e90bfd0693e18d0
user <user@WIN-HR116O5DJ9U>
2023-05-29 11:01:47 +0500

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                count += kub(i, n - i);
+            }
+            Console.WriteLine(count + 1);
+            // Console.ReadKey();
+
+        }
+
+        static int kub(int a, int b)
+        {
+            if (b == 0)
+                return 1;
+
+            int count = 0;
+            for (int i = 0; i < a; i++)
+
+                if (b - i >= 0)
+                    count += kub(i, b - i);
+            return count;
+
+        }
+
+    }
+}
